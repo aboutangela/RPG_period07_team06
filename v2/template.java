4025e19ae239315a1fdd2a298967627d23116e5a
angela <ali18@stuy.edu>
2017-12-04 19:31:04 -0500

    v2

---
+++ v2/template.java
@@ -0,0 +1,17 @@
+/** 
+ *  
+ *  
+ */
+
+public class Test {
+
+    /** 
+     *  
+     *  
+     */
+    public static void main(String[] commandLine) {
+        System.out.println( System.lineSeparator()
+                          );
+
+    }
+}
