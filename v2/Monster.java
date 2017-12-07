cc741053bc462f2e252e83e5f2e37e24beb8ac50
angela <ali18@stuy.edu>
2017-12-06 20:12:27 -0500

    modified random in Monster

--- v2/Monster.java
+++ v2/Monster.java
@@ -2,7 +2,7 @@ public class Monster extends Character{
 
 	public Monster(){
 		health = 150;
-		strength = 20 + (int) Math.random() * 45;
+		strength = (int) (20 + Math.random() * 45);
 		defense = 20;
 		attack = 1.0;
 	}
